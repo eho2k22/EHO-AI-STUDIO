c547d61b4bbdf324abaf20d90988b4921b2bdbfe
eho2k22 <104375668+eho2k22@users.noreply.github.com>
2023-01-25 19:47:41 -0800

    Add files via upload
    Adding Night mode.
    
    Adding failover handling  in case Davinci-003 model is unavailable

--- eho-ai-studio/main.py
+++ eho-ai-studio/main.py
@@ -16,6 +16,8 @@ usertemp=""
 answer = ""
 engine_temperature = 0.5
 
+
+
 #def answer_question(question):
  #   response = openai.Completion.create(
   #      engine="text-davinci-003",
@@ -40,7 +42,6 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
 
-
     if request.method == "POST":
         print(request.form["prompt"])
         userprompt = request.form["prompt"]
@@ -53,6 +54,13 @@ def index():
 
         openai.api_key = userapikey
 
+        available_003 = False
+        models = openai.Model.list()
+        print("Available Models are: ")
+        for model in models['data']:
+            print(model.id)
+            if (model.id == "text-davinci-003"):
+                available_003 = True
 
         if (usertemp == "Standard"):
             engine_temperature = 0.5
@@ -63,17 +71,35 @@ def index():
 
         print("Temperature set to " + usertemp + "at : " + str(engine_temperature))
 
-        try: 
-            response = openai.Completion.create(
-                engine="text-davinci-003",
-                prompt=userprompt,
-                max_tokens=2048,
-                n=1,
-                stop=None,
-                temperature=engine_temperature,
-            )
-        except:
-            return render_template("error.html", userapikey=userapikey)
+        if (available_003):
+            try: 
+                print("Davince-003 is available !! ")
+                response = openai.Completion.create(
+                    engine="text-davinci-003",
+                    prompt=userprompt,
+                    max_tokens=2048,
+                    n=1,
+                    stop=None,
+                    temperature=engine_temperature,
+                )
+            except:
+                print("something went wrong while processing your question.. ")
+                return render_template("error.html", userapikey=userapikey)
+
+        else:
+            try: 
+                response = openai.Completion.create(
+                    engine="text-davinci-002",
+                    prompt=userprompt,
+                    max_tokens=2048,
+                    n=1,
+                    stop=None,
+                    temperature=engine_temperature,
+                )
+            except:
+                print("something went wrong while processing your question.. ")
+                return render_template("error.html", userapikey=userapikey)
+    
 
         answer = response.choices[0].text
     
@@ -83,18 +109,97 @@ def index():
         #answer = answer_question(userprompt)
         print("ANSWER IS: " + answer)
 
-        return render_template("answer.html", userprompt=userprompt, answer=answer)
+        return render_template("answer.html", userprompt=userprompt, answer=answer, userapikey=userapikey)
     
     return render_template('index.html')
 
 
-@app.route('/night_mode')
+@app.route('/night_mode', methods=["GET", "POST"])
 def night_mode():
+    if request.method == "POST":
+        print(request.form["prompt"])
+        userprompt = request.form["prompt"]
+        userapikey = request.form["apikey"]
+        usertemp = request.form["temperature"]
+
+        print("NIGHT MODE: Your Question is: "+ userprompt)
+        print("Your API Key is:" + userapikey)
+        print("Your Requested Temperature is: " + usertemp)
+
+        openai.api_key = userapikey
+
+        available_003 = False
+        models = openai.Model.list()
+        print("Available Models are: ")
+        for model in models['data']:
+            print(model.id)
+            if (model.id == "text-davinci-003"):
+                available_003 = True
+
+
+        if (usertemp == "Standard"):
+            engine_temperature = 0.5
+        if (usertemp == "High"):
+            engine_temperature = 1.0
+        if (usertemp == "Ultra"):
+            engine_temperature = 1.5
+
+        print("Temperature set to " + usertemp + "at : " + str(engine_temperature))
+
+        if (available_003):
+            try: 
+                print("Davince-003 is available !! ")
+                response = openai.Completion.create(
+                    engine="text-davinci-003",
+                    prompt=userprompt,
+                    max_tokens=2048,
+                    n=1,
+                    stop=None,
+                    temperature=engine_temperature,
+                )
+            except:
+                print("something went wrong while processing your question.. ")
+                return render_template("error.html", userapikey=userapikey)
+
+        else:
+            try: 
+                response = openai.Completion.create(
+                    engine="text-davinci-002",
+                    prompt=userprompt,
+                    max_tokens=2048,
+                    n=1,
+                    stop=None,
+                    temperature=engine_temperature,
+                )
+            except:
+                print("something went wrong while processing your question.. ")
+                return render_template("error.html", userapikey=userapikey)
+    
+
+        answer = response.choices[0].text
+    
+        if userprompt.lower() == "exit":
+            return render_template('night_mode.html')
+        
+        #answer = answer_question(userprompt)
+        print("ANSWER IS: " + answer)
+
+        return render_template("answer_nm.html", userprompt=userprompt, answer=answer)
+    
     return render_template('night_mode.html')
 
 @app.route('/gallery')
 def gallery():
     return render_template('gallery.html')
+
+@app.route('/answer_nm')
+def answer_nm():
+    return render_template('answer_nm.html')
+
+@app.route('/gallery_nm')
+def gallery_nm():
+    return render_template('gallery_nm.html')
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8081, debug=True)
